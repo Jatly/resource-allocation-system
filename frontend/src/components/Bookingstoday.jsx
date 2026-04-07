a6057f754d4aa9f468d861880370b39901b4d038
JATLY <131124699+Jatly@users.noreply.github.com>
2026-04-07 17:19:04 +0530

    p

--- frontend/src/components/Bookingstoday.jsx
+++ frontend/src/components/Bookingstoday.jsx
@@ -64,7 +64,7 @@ const Bookingstoday = () => {
 
                 <td>
                   {new Date(item.startTime).toLocaleString("en-IN", {
-                    hour: "2-digit",
+                    hour: "numeric",
                     minute: "2-digit",
                     hour12: true,
                   })}
@@ -72,7 +72,7 @@ const Bookingstoday = () => {
 
                 <td>
                   {new Date(item.endTime).toLocaleString("en-IN", {
-                    hour: "2-digit",
+                    hour: "numeric",
                     minute: "2-digit",
                     hour12: true,
                   })}
